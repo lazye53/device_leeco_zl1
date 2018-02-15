6eee8e0c1892102a5c2f7e3c35f2fda6cc83da2c
Gabr0 <gabronogue@gmail.com>
2018-02-15 17:56:03 +0200

    Aosipify

--- aosip.mk
+++ aosip.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The The LineageOS Project
+# Copyright (C) 2018 The The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,13 +16,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from zl1 device
+# Inherit from zl1/device
 $(call inherit-product, device/leeco/zl1/device.mk)
 
-# Inherit some common aicp stuff
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+# Inherit some common aosip stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-PRODUCT_NAME := aicp_zl1
+PRODUCT_NAME := aosip_zl1
 PRODUCT_DEVICE := zl1
 PRODUCT_MANUFACTURER := LeEco
 PRODUCT_BRAND := LeEco
@@ -35,18 +34,9 @@ TARGET_VENDOR_DEVICE_NAME := le_zl1
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=le_zl1 PRODUCT_NAME=LePro3
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=LeEco/ZL1_NA/le_zl1:6.0.1/WEXNAOP5802101261S/letv01261206:user/release-keys \
-    PRIVATE_BUILD_DESC="le_zl1-user 6.0.1 WEXNAOP5802101261S eng.letv.20170126.120318 release-keys"
+    BUILD_FINGERPRINT="LeEco/ZL1_CN/le_zl1:6.0.1/WAXCNFN5902801241S/17:user/release-keys" \
+    PRIVATE_BUILD_DESC="le_zl1-user 6.0.1 WAXCNFN5902801241S 17 release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
 
 TARGET_VENDOR := leeco
-
-# AICP Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
--include vendor/aicp/configs/bootanimation.mk
